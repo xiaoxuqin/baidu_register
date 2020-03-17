6210acbf34d2f97ee12b2c27503f035850c2dcde
xiaoxuqin <2837303093@qq.com>
2020-03-17 15:49:57 +0800

    test_grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,21 +19,21 @@ module.exports = function (grunt) {
         uglify:{
             'dist/index.js':'index.js'
         },
-        copy:{
-            html:{
-                src:'index.html',
-                dest:'./dist/index.html'
-            }
-        },
+        // copy:{
+        //     html:{
+        //         src:'index.html',
+        //         dest:'./dist/index.html'
+        //     }
+        // },
         // useminPrepare:{
         //     html:'index.html',
         //     options:{
         //         dest:'dist'
         //     }
         // },
-        usemin:{
-            html:['dist/index.html']
-        }
+        // usemin:{
+        //     html:['dist/index.html']
+        // }
     });
     
     grunt.loadNpmTasks('grunt-contrib-clean');
@@ -41,10 +41,11 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-usemin');
+    // grunt.loadNpmTasks('grunt-contrib-copy');
+    // grunt.loadNpmTasks('grunt-usemin');
 
 
 
-    grunt.registerTask('default', ['clean', 'copy',  'uglify', 'usemin', 'htmlmin', 'cssmin']);
+    grunt.registerTask('default', ['clean', 'uglify', 'htmlmin', 'cssmin']);
+    // grunt.registerTask('default', ['clean', 'copy',  'uglify', 'usemin', 'htmlmin', 'cssmin']);
 }
