ce5f41fa151fb138b32128f0f202da29b65fec61
xiaoxuqin <2837303093@qq.com>
2020-03-17 17:42:46 +0800

    success

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,35 +17,18 @@ module.exports = function (grunt) {
             'dist/index.css':'index.css'
         },
         uglify:{
-            'dist/index.js':'index.js'
+            release: {
+                files: {
+                    "dist/index.min.js": "index.js"
+                }
+            }
         },
-        // copy:{
-        //     html:{
-        //         src:'index.html',
-        //         dest:'./dist/index.html'
-        //     }
-        // },
-        // useminPrepare:{
-        //     html:'index.html',
-        //     options:{
-        //         dest:'dist'
-        //     }
-        // },
-        // usemin:{
-        //     html:['dist/index.html']
-        // }
     });
     
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    
-    // grunt.loadNpmTasks('grunt-contrib-copy');
-    // grunt.loadNpmTasks('grunt-usemin');
 
-
-
-    grunt.registerTask('default', ['clean', 'uglify', 'htmlmin', 'cssmin']);
-    // grunt.registerTask('default', ['clean', 'copy',  'uglify', 'usemin', 'htmlmin', 'cssmin']);
+    grunt.registerTask('default', ['clean', 'uglify', 'htmlmin', 'cssmin']);    
 }
